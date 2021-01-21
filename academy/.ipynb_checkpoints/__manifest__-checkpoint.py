34dc39dd647a60ec817e8d80eb8a1e01adaa1bac
thymargo <thymargo@gmail.com>
2021-01-21 01:14:36 +0000

    [ADD] academy

---
+++ academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    
+    'name': 'Odoo Academy',
+    'summary': """Academy app to manage Training""",
+    'description':"""
+        Academy Module to manage Training: 
+        - Courses
+        - Session
+        - Attendees
+    """,
+    'author': 'Odoo',
+    'website':'https://www.odoo.com',
+    'category': 'Training',
+    'version':'0.1',
+    'depends':['base'],
+    'data':[
+        #'security/academy_security.xml',
+        #'security/ir.model.access.csv',
+       # 'views/academy_menuitems.xml',
+        #'views/course_views.xml',
+    ],
+    'demo':[
+        'demo/academy_demo.xml'
+    ],
+}
