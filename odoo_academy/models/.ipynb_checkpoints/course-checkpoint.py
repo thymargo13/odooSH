89c286477d9b5caaaaad7213a6f7cf3b09808e94
thymargo <thymargo@gmail.com>
2021-01-26 01:22:54 +0000

    [ADD] wizard

--- odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import  UserError, ValidationError
 
 class Course(models.Model):
     _name = 'academy.course'
@@ -16,4 +17,32 @@ class Course(models.Model):
                             copy=False)
     
     active = fields.Boolean(string='Active', default=True)
-                            
+    
+    base_price =  fields.Float(string='Base  Price', default=0.00)
+    additional_fee = fields.Float(string='Additional Fee', default=0.00)
+    total_price =  fields.Float(string='Total Price',readonly=True)
+    
+    #one to many -->  link  with  session
+    session_ids =  fields.One2many(comodel_name='academy.session',
+                                  inverse_name='course_id',
+                                  string='Sessions')
+
+    
+    #if base price / additional fee change trigger the method.
+    #Add Exception, if base price <0 -->  error
+    @api.onchange('base_price', 'additional_fee')
+    def _onchange_total_price(self):
+         #Add Exception, if base price <0 -->  error
+        if self.base_price<0.00:
+            raise UserError('Base Price  cannot be set as Negative.')
+         #set total price = base price + additional fee
+        self.total_price =  self.base_price +  self.additional_fee
+        
+    @api.constrains('additional_fee')
+    def _check_additional_fee(self):
+        for record in self:
+            #  set additional_fee always greater than 10.00
+            if  record.additional_fee < 10.00:
+                raise ValidationError('Additional Fee cannot be less than 10.00: %s' % record.additional_fee)
+                
+        
---
+++ odoo_academy/models/session.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from  datetime import timedelta
+from odoo.exceptions import  UserError, ValidationError
+
+class Session(models.Model):
+    _name = 'academy.session'
+    _description ='Session Info'
+    
+    course_id = fields.Many2one(comodel_name='academy.course',
+                              string='Course',
+                              ondelete='cascade',
+                              required=True)
+    #related field
+    name = fields.Char(string = 'Title', related='course_id.name')
+    instructor_id  =  fields.Many2one(comodel_name='res.partner',string='Instructor')
+    student_ids =  fields.Many2many(comodel_name='res.partner', string='Students')
+    start_date =   fields.Date(string='Start Date',
+                              default=fields.Date.today)
+    duration = fields.Integer(string='Session Days',
+                              default=1)
+    end_date =   fields.Date(string='End Date',
+                             compute='_compute_end_date',
+                             inverse='_inverse_end_date',
+                             store=True)
+    state = fields.Selection(string='States',
+                            selection=[('draft','Draft'),
+                                       ('open','In Progress'),
+                                       ('done','Done'),
+                                       ('complete','Complete')],
+                            default='draft',
+                            requirement=True)
+    total_price = fields.Float(string='Total Price',
+                              related='course_id.total_price')
+    
+    @api.depends('start_date','duration')
+    def _compute_end_date(self):
+        for record in self:
+            if not (record.start_date and record.duration):
+                record.end_date = record.start_date
+            else:
+                duration =  timedelta(days=record.duration)
+                record.end_date =  record.start_date + duration
+    
+    def  _inverse_end_date (self):
+        for record in self:
+            if record.start_date  and  record.end_date:
+                # ().days  --> convert to  days
+                # +1 --> Monday  -  Thurdays  = 4days.  but  we consider  it  as  5 days
+                record.duration = (record.end_date - record.start_date).days + 1
+            else:
+                continue
